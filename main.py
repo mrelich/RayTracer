01ddcf0c167cbff383bad335137a9811878ca062
mrelich <mrelich6@gmail.com>
2014-09-10 17:20:29 +0900

    Structure Added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 # This will be the 'executable' that will draw the ray tracing for the ice #
 # block. Will add some options in the future, but at first we consider a   #
 # simple case.                                                             #
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+
