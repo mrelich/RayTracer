7c8576ea0142724a32fd6ef0da024f9e708c6a33
mrelich <mrelich6@gmail.com>
2014-09-10 17:47:57 +0900

    Updating

--- cube.py
+++ cube.py
@@ -3,3 +3,28 @@
 # This will be the virtual ice block and I wanted to have it as a class #
 # so I can have methods to tell if the ray is still in the ice block.   #
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-#
+
+from visual import *
+
+class cube:
+    
+
+    #----------------------------------------#
+    # Constructor
+    #----------------------------------------#
+    def __init__(self,x,y,z,L,H):
+
+        # Save generic characteristics
+        self.xpos   = x
+        self.ypos   = y
+        self.zpos   = z
+        self.length = L
+        self.height = H
+
+        # Create the box
+        self.box = box(pos=(x,y,z),
+                       length=L,
+                       height=H,
+                       width=0,
+                       color=(0,1.5,1))
+        
--- main.py
+++ main.py
@@ -6,3 +6,35 @@
 # simple case.                                                             #
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
 
+#-------------------------------------#
+# Import necessaries
+#-------------------------------------#
+
+from visual import *
+from cube import *
+
+#-------------------------------------#
+# Setup the environment
+#-------------------------------------#
+
+window = display(title='Ray Tracing', x=0, y=0, 
+                 width=600, height=600,
+                 center=(0,0,0), background=(1,1,1),
+                 range=1)
+
+#-------------------------------------#
+# Make the ice block
+#-------------------------------------#
+
+# Dimensions
+length = 1   # [m]
+height = 0.3 # [m]
+width  = 0.3 # [m] This is not used. Just 2D
+
+# Locations
+icex = 0
+icey = 0
+icez = 0
+
+# make the cube
+iceblock = cube(icex,icey,icez,length,height)
